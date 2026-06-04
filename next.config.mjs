34ba4853f4537ec503add1821f08401b19f6a013
dmitriydiachuk2009@mail.ru <cxdpq99kvh@privaterelay.appleid.com>
2026-06-05 00:53:07 +0500

    Fix: rename next.config.ts to next.config.mjs for Next.js 14

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    unoptimized: true, // Важно для Netlify
+    unoptimized: true, // Обязательно для Netlify
   },
 };
 
